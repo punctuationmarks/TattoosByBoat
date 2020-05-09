0b7a21b1c0137d64f529197470eef9479b6025cb
punctuationmarks <punctuationmarks@protonmail.com>
2020-05-09 11:21:55 -0400

    removed extended network

--- src/components/UserProfile/UserInfo.js
+++ src/components/UserProfile/UserInfo.js
@@ -6,45 +6,50 @@ const UserInfo = () => (
   <UserConsumer>
     {value => (
       <>
+        <Row>
 
-        <Col>
-          <div className="bioDiv">
+          <Col>
+            <div className="bioDiv">
+              <br />
+              <h3>{value.username}</h3>
+              <br />
+              <Image src={value.profile_image} alt="profile image" />
+            </div>
             <br />
-            <h3>{value.username}</h3>
-            <br />
-            <Image src={value.profile_image} alt="profile image"/>
-           </div>
-          <br />
-          <p>
-            View My:
+            <p>
+              View My:
             </p>
-          <p>
-            <a href={value.instagram} target="_blank" rel="noopener noreferrer">
-              Insta</a> |
+            <p>
+              <a href={value.instagram} target="_blank" rel="noopener noreferrer">
+                Insta</a> |
            <a href={value.shop_website} target="_blank" rel="noopener noreferrer">
-              Shop</a> |
+                Shop</a> |
            <a href={value.shop_instagram} target="_blank" rel="noopener noreferrer">
-              Shop's Insta</a>
-          </p>
-        </Col>
+                Shop's Insta</a>
+            </p>
+          </Col>
 
-        <Col className="bioDiv">
-          <br />
-          <br />
-          <br />
-          <p>"{value.bio}"</p>
-          <p className="bioP">
-            {value.city},</p>
-          <p className="bioP">
-            {value.state} {value.country}</p>
-          <br />
-          <br />
-        </Col>
-{/* issues with css currently, need to figure out best way to load for mobile, since the text falls out of the box
+          <Col className="bioDiv">
+            <br />
+            <br />
+            <br />
+            <p>"{value.bio}"</p>
+            <p className="bioP">
+              {value.city},</p>
+            <p className="bioP">
+              {value.state} {value.country}</p>
+            <br />
+            <br />
+          </Col>
+
+        </Row>
+        {/* <Row>
+
           <div className="extendedNetwork">
             {value.username} is in your extended network
-          </div> */}
+          </div>
 
+        </Row> */}
       </>
     )}
   </UserConsumer>
