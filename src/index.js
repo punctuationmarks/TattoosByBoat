24ecae8fbde9a5791e0430b908e5dc9395c1a1a9
punctuationmarks <punctuationmarks@protonmail.com>
2020-10-17 13:45:09 -0400

    removed service worker

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import UserProvider from './providers/UserProvider'
-import 'bootstrap/dist/css/bootstrap.css';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter, } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import UserProvider from "./providers/UserProvider";
+import "bootstrap/dist/css/bootstrap.css";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <UserProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </UserProvider>, 
-  document.getElementById('root')
+  </UserProvider>,
+  document.getElementById("root")
 );
-serviceWorker.register();
+serviceWorker.unregister();
